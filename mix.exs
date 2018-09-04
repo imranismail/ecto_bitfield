b8b1cc5c1c5e1117f1bc67bcf082591cef0cdeda
Imran Ismail <imran.codely@gmail.com>
2018-09-04 16:24:15 +0800

    Prep for release

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule EctoBitfield.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,9 +23,26 @@ defmodule EctoBitfield.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 2.1", optional: true}
+      {:ecto, "~> 2.1", optional: true},
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description do
+    """
+    A tool to use Bitfields with Ecto schemas
+    """
+  end
+
+  defp package do
+    [
+      name: :ecto_bitfield,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Imran Ismail"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/imranismail/ecto_bitfield"}
     ]
   end
 end
