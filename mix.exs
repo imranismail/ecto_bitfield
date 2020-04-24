b25bf7e8913d55650e239cff2871e9f7798c3492
Imran Ismail <imran.codely@gmail.com>
2020-04-24 23:56:03 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule EctoBitfield.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 2.1", optional: true},
+      {:ecto, "~> 2.0 or ~> 3.0", optional: true},
       {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
